f5db141a4ed58a2cf6ed33b347317f0cb3507d80
Chris <cachou@umich.edu>
2018-11-18 19:10:08 -0500

    ng working southu

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 var app = angular.module('h2a2', []);
 
 app.controller('southUbarInfo',[ '$scope', '$http', function($scope, $http) {
+
+  $scope.barResults = {};
   //$scope.barResults = $http.get("../config.json");
-  $http.get('../config.json').then(function(response) {
-   $scope.barResults = response.data;
+  $http.get('http://localhost:8000/config.json').then(function(response) {
+    //console.log(response.data);
+    $scope.barResults = response.data[0]["southU"];
+    console.log($scope.barResults);
   });
-  console.log($scope.barResults);
 }]);
